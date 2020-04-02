098bf378e308517e505aa1fdb197976522155069
elywin <lukeeraelywin@gmail.com>
2020-04-02 20:22:41 +0300

    created functions for addition,subtraction,multipication and divsion

--- func.js
+++ func.js
@@ -1,16 +1,48 @@
 
 let tapButton = document.getElementById("mult");
+let tapButton = document.getElementById("div");
+let tapButton = document.getElementById("plus");
+let tapButton = document.getElementById("minus");
+
+
 
 multi =()=>{
-  let num11 = Number(document.getElementById("2").value);
-  let num22 =   Number(document.getElementById("3").value);
+  let num11 = Number(document.getElementById("").value);
+  let num22 =   Number(document.getElementById("").value);
   return  num11 * num22;
    //alert(result);
   // alert(num11 * num22);
 
 };
 
-tapButton.onclick = ()=>{
- let result=multi();  
- document.getElementById("text1").value="Answer=" +" "+ result;
+divi =()=>{
+  let num11 = Number(document.getElementById("").value);
+  let num22 =   Number(document.getElementById("").value);
+  return  num11 / num22;
+   //alert(result);
+  // alert(num11 * num22);
+
 };
+
+add =()=>{
+  let num11 = Number(document.getElementById("").value);
+  let num22 =   Number(document.getElementById("").value);
+  return  num11 + num22;
+   //alert(result);
+  // alert(num11 * num22);
+
+};
+
+sub =()=>{
+  let num11 = Number(document.getElementById("").value);
+  let num22 =   Number(document.getElementById("").value);
+  return  num11 - num22;
+   //alert(result);
+  // alert(num11 * num22);
+
+};
+
+// tapButton.onclick = ()=>{
+//  let result=multi();  
+//  document.getElementById("text1").value="Answer=" +" "+ result;
+// };
