0679c09b7772951fa5e3287ec3c41aae06a7ee7b
elywin <lukeeraelywin@gmail.com>
2020-04-03 12:16:53 +0300

    Created on click functions for the buttons adding them to the textarea

--- func.js
+++ func.js
@@ -1,48 +1,159 @@
 
-let tapButton = document.getElementById("mult");
-let tapButton = document.getElementById("div");
-let tapButton = document.getElementById("plus");
-let tapButton = document.getElementById("minus");
+//number 1
+let tapnum1 = document.getElementById("1");
+tapnum1.onclick =()=>{
+ 
+  let num11 = Number(document.getElementById("1").value);
+  document.getElementById("text1").value += num11;
+ };
 
-
-
-multi =()=>{
-  let num11 = Number(document.getElementById("").value);
-  let num22 =   Number(document.getElementById("").value);
-  return  num11 * num22;
-   //alert(result);
-  // alert(num11 * num22);
-
+ //number 2
+let tapnum2 = document.getElementById("2");
+tapnum2.onclick =()=>{
+ 
+ let num11 = Number(document.getElementById("2").value);
+ document.getElementById("text1").value += num11;
 };
 
-divi =()=>{
-  let num11 = Number(document.getElementById("").value);
-  let num22 =   Number(document.getElementById("").value);
-  return  num11 / num22;
-   //alert(result);
-  // alert(num11 * num22);
 
+//number 3
+let tapnum3 = document.getElementById("3");
+tapnum3.onclick =()=>{
+ 
+ let num11 = Number(document.getElementById("3").value);
+ document.getElementById("text1").value += num11;
 };
 
-add =()=>{
-  let num11 = Number(document.getElementById("").value);
-  let num22 =   Number(document.getElementById("").value);
-  return  num11 + num22;
-   //alert(result);
-  // alert(num11 * num22);
-
+//number 4 
+let tapnum4 = document.getElementById("4");
+tapnum4.onclick =()=>{
+ 
+ let num11 = Number(document.getElementById("4").value);
+ document.getElementById("text1").value += num11;
 };
 
-sub =()=>{
-  let num11 = Number(document.getElementById("").value);
-  let num22 =   Number(document.getElementById("").value);
-  return  num11 - num22;
-   //alert(result);
-  // alert(num11 * num22);
-
+//number 5
+let tapnum5 = document.getElementById("5");
+tapnum5.onclick =()=>{
+ 
+ let num11 = Number(document.getElementById("5").value);
+ document.getElementById("text1").value += num11;
 };
 
-// tapButton.onclick = ()=>{
-//  let result=multi();  
-//  document.getElementById("text1").value="Answer=" +" "+ result;
+
+//number 6
+let tapnum6 = document.getElementById("6");
+tapnum6.onclick =()=>{
+ 
+ let num11 = Number(document.getElementById("6").value);
+ document.getElementById("text1").value += num11;
+};
+
+//number 7
+let tapnum7 = document.getElementById("7");
+tapnum7.onclick =()=>{
+ 
+ let num11 = Number(document.getElementById("7").value);
+ document.getElementById("text1").value += num11;
+};
+
+
+//number 8
+let tapnum8 = document.getElementById("8");
+tapnum8.onclick =()=>{
+ 
+ let num11 = Number(document.getElementById("8").value);
+ document.getElementById("text1").value += num11;
+};
+
+//number 9
+let tapnum9 = document.getElementById("9");
+tapnum9.onclick =()=>{
+ 
+ let num11 = Number(document.getElementById("9").value);
+ document.getElementById("text1").value += num11;
+};
+
+//number 0
+let tapnum0 = document.getElementById("0");
+tapnum0.onclick =()=>{
+ 
+ let num11 = Number(document.getElementById("0").value);
+ document.getElementById("text1").value += num11;
+};
+
+//subtraction button
+let tapminus = document.getElementById("minus");
+tapminus.onclick =()=>{
+ 
+ let num11 = document.getElementById("minus").value;
+ document.getElementById("text1").value += num11;
+};
+
+//addition button
+let tapplus = document.getElementById("plus");
+tapplus.onclick =()=>{
+ 
+ let num11 = document.getElementById("plus").value;
+ document.getElementById("text1").value += num11;
+};
+
+
+//multiplication button
+let tapmult = document.getElementById("mult");
+tapmult.onclick =()=>{
+ 
+ let num11 = document.getElementById("mult").value;
+ document.getElementById("text1").value += num11;
+};
+
+
+//division button
+let tapdiv = document.getElementById("div");
+tapdiv.onclick =()=>{
+ 
+ let num11 = document.getElementById("div").value;
+ document.getElementById("text1").value += num11;
+};
+
+//point button
+let tappoint = document.getElementById("point");
+tappoint.onclick =()=>{
+ 
+ let num11 = document.getElementById("point").value;
+ document.getElementById("text1").value += num11;
+};
+
+//leftbrace button
+let tapLbr = document.getElementById("Lbr");
+tapLbr.onclick =()=>{
+ 
+ let num11 = document.getElementById("Lbr").value;
+ document.getElementById("text1").value += num11;
+};
+
+//righttbrace button
+let tapRbr = document.getElementById("Rbr");
+tapRbr.onclick =()=>{
+ 
+ let num11 = document.getElementById("Rbr").value;
+ document.getElementById("text1").value += num11;
+};
+
+//percentage button
+let tapperc = document.getElementById("perc");
+tapperc.onclick =()=>{
+ 
+ let num11 = document.getElementById("perc").value;
+ document.getElementById("text1").value += num11;
+};
+
+// multi =()=>{
+// let num11 =  tapnum;
+// let num22 = tapnum;
+//   // let num11 = Number(document.getElementById("num").value);
+//   // let num22 =   Number(document.getElementById("num").value);
+//  // return  num11 * num22;
+//    //alert(result);
+//    alert(num11 * num22);
 // };
