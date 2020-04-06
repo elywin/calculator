0d6d2adba0dc560e1b5f5a9ce0e08d5243ae9bdb
elywin <lukeeraelywin@gmail.com>
2020-04-06 15:59:41 +0300

    created function to input decimal point

--- func.js
+++ func.js
@@ -1,4 +1,4 @@
-//create calculator object holding data to constract valid expression
+//calculator object holding data to constract valid expression
 const calculator ={
   displayValue:'0',
   firstOperand:null,
@@ -6,22 +6,30 @@ const calculator ={
   operator:null,
 };
 
-//create function to display values of clicked buttons.
+// function to display values of clicked buttons.
 inputDigit = (digit)=>{
   const{displayValue} = calculator;
   //overwriting displayValue if current value is 0 otherwise append it 
   calculator.displayValue = displayValue ==='0'? digit : displayValue + digit;
 }
 
+//function to input decimal point
+inputDecimal = (dot)=>{
+  //if the displayValue does not contain a decimal point
+  if(!calculator.displayValue.includes(dot)){
+    //append the decimal point
+    calculator.displayValue += dot;
+  }
+}
 
-//created a function updateDisplay to display value on the calculator screen
+// function updateDisplay to display value on the calculator screen
 updateDisplay=()=>{
   const display = document.querySelector('.calculator-screen');
   display.value = calculator.displayValue;
 }
 //call the function
 updateDisplay();
-//create click event listener on the calculator keys.
+// click event listener on the calculator keys.
 const keys= document.querySelector('.calculator-keys');
 keys.addEventListener('click',(event)=>{
   const{target} =event;
@@ -37,6 +45,8 @@ keys.addEventListener('click',(event)=>{
 
   if(target.classList.contains('decimal')){
     console.log('decimal',target.value);
+    inputDecimal(target.value);
+    updateDisplay();
     return;
   }
 
